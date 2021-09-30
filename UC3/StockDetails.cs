e7ba55c650d45a5db1dc960fde2939d43bd4c649
asomwanshi37 <asomwanshi37@gmail.co.com>
2021-09-30 17:58:37 +0530

    Stock Account Management Program

---
+++ UC3/StockDetails.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Stock_Account_Management
+{
+    class StockDetails
+    {
+        public List<Stock1> Stock1;
+        public List<Stock2> Stock2;
+        public List<Stock3> Stock3;
+    }
+    class Stock1
+    {
+        public string name;
+        public int NoOfShares;
+        public double SharePrice;
+
+    }
+    class Stock2
+    {
+        public string name;
+        public int NoOfShares;
+        public double SharePrice;
+
+    }
+    class Stock3
+    {
+        public string name;
+        public int NoOfShares;
+        public double SharePrice;
+
+    }
+}
---
+++ UC3/StockPortfolio.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Stock_Account_Management
+{
+    class StockPortfolio
+    {
+        internal static double StockValue(double SharePrice, int NoOfShares)
+        {
+            double result = SharePrice * NoOfShares;
+            return result;
+        }
+    }
+}
